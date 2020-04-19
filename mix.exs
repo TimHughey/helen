36a116d02c802b6cd7c0f381d4189665047f346d
Tim Hughey <timhughey@mac.com>
2020-04-19 14:20:12 -0400

    License Specific

--- mix.exs
+++ mix.exs
@@ -1,23 +1,9 @@
 defmodule Helen.Mixfile do
+  # Helen Copyright (C) 2020  Tim Hughey (thughey)
+
   @moduledoc """
     Mix file defining Helen
   """
-
-  # Helen
-  # Copyright (C) 2020  Tim Hughey (thughey)
-  #
-  # This program is free software: you can redistribute it and/or modify
-  # it under the terms of the GNU General Public License as published by
-  # the Free Software Foundation, either version 3 of the License, or
-  # (at your option) any later version.
-  #
-  # This program is distributed in the hope that it will be useful,
-  # but WITHOUT ANY WARRANTY; without even the implied warranty of
-  # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  # GNU General Public License for more details.
-  #
-  # You should have received a copy of the GNU General Public License
-  # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
   use Mix.Project
 
@@ -159,7 +145,7 @@ defmodule Helen.Mixfile do
                 license* CHANGELOG* changelog*),
       links: %{"GitHub" => "https://github.com/TimHugheyhelen"},
       maintainers: ["Tim Hughey"],
-      licenses: ["Apache-2.0"],
+      licenses: ["LGPL-3.0-or-later"],
       homepage_url: "https://www.wisslanding.com"
     ]
   end
