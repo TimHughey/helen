269418ac48dd5a272c6fa6d46ddc934aa7d31f07
Tim Hughey <timhughey@mac.com>
2020-05-27 17:14:37 -0400

    Updated Production SQL Structure

--- priv/repo/structure-prod.sql
+++ priv/repo/structure-prod.sql
@@ -133,42 +133,6 @@ ALTER SEQUENCE public.dutycycle_state_id_seq OWNED BY public.dutycycle_state.id;
 
 
 --
--- Name: message; Type: TABLE; Schema: public; Owner: -
---
-
-CREATE TABLE public.message (
-    id bigint NOT NULL,
-    direction character varying(15) NOT NULL,
-    src_host text DEFAULT ' '::text NOT NULL,
-    msgpack bytea DEFAULT '\x00'::bytea NOT NULL,
-    json text DEFAULT ' '::text NOT NULL,
-    dropped boolean DEFAULT false NOT NULL,
-    keep_for_testing boolean DEFAULT false NOT NULL,
-    inserted_at timestamp(0) without time zone NOT NULL,
-    updated_at timestamp(0) without time zone NOT NULL
-);
-
-
---
--- Name: message_id_seq; Type: SEQUENCE; Schema: public; Owner: -
---
-
-CREATE SEQUENCE public.message_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
---
--- Name: message_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
---
-
-ALTER SEQUENCE public.message_id_seq OWNED BY public.message.id;
-
-
---
 -- Name: pwm; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -186,13 +150,11 @@ CREATE TABLE public.pwm (
     ttl_ms integer DEFAULT 60000,
     reading_at timestamp without time zone,
     last_seen_at timestamp without time zone,
-    metric_at timestamp without time zone,
-    metric_freq_secs integer DEFAULT 60,
     discovered_at timestamp without time zone,
     last_cmd_at timestamp without time zone,
     inserted_at timestamp(0) without time zone NOT NULL,
     updated_at timestamp(0) without time zone NOT NULL,
-    runtime_metrics jsonb DEFAULT '{"cmd_rt": true, "external_update": false}'::jsonb NOT NULL
+    sequence character varying(255) DEFAULT 'none'::character varying
 );
 
 
@@ -408,9 +370,43 @@ CREATE TABLE public.sensor (
     inserted_at timestamp without time zone NOT NULL,
     updated_at timestamp without time zone NOT NULL,
     metric_freq_secs integer DEFAULT 60,
-    metric_at timestamp(0) without time zone DEFAULT NULL::timestamp without time zone,
-    runtime_metrics jsonb DEFAULT '{"cmd_rt": true, "external_update": false}'::jsonb NOT NULL
+    metric_at timestamp(0) without time zone DEFAULT NULL::timestamp without time zone
 );
+
+
+--
+-- Name: sensor_device; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.sensor_device (
+    id bigint NOT NULL,
+    device character varying(255) NOT NULL,
+    host character varying(255) NOT NULL,
+    dev_latency_us integer DEFAULT 0 NOT NULL,
+    last_seen_at timestamp without time zone,
+    discovered_at timestamp without time zone,
+    inserted_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL
+);
+
+
+--
+-- Name: sensor_device_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.sensor_device_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: sensor_device_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.sensor_device_id_seq OWNED BY public.sensor_device.id;
 
 
 --
@@ -746,13 +742,6 @@ ALTER TABLE ONLY public.dutycycle_state ALTER COLUMN id SET DEFAULT nextval('pub
 
 
 --
--- Name: message id; Type: DEFAULT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.message ALTER COLUMN id SET DEFAULT nextval('public.message_id_seq'::regclass);
-
-
---
 -- Name: pwm id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -785,6 +774,13 @@ ALTER TABLE ONLY public.remote_profile ALTER COLUMN id SET DEFAULT nextval('publ
 --
 
 ALTER TABLE ONLY public.sensor ALTER COLUMN id SET DEFAULT nextval('public.sensor_id_seq'::regclass);
+
+
+--
+-- Name: sensor_device id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.sensor_device ALTER COLUMN id SET DEFAULT nextval('public.sensor_device_id_seq'::regclass);
 
 
 --
@@ -868,14 +864,6 @@ ALTER TABLE ONLY public.dutycycle_state
 
 
 --
--- Name: message message_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.message
-    ADD CONSTRAINT message_pkey PRIMARY KEY (id);
-
-
---
 -- Name: pwm_cmd pwm_cmd_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -913,6 +901,14 @@ ALTER TABLE ONLY public.remote_profile
 
 ALTER TABLE ONLY public.schema_migrations
     ADD CONSTRAINT schema_migrations_pkey PRIMARY KEY (version);
+
+
+--
+-- Name: sensor_device sensor_device_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.sensor_device
+    ADD CONSTRAINT sensor_device_pkey PRIMARY KEY (id);
 
 
 --
@@ -1009,20 +1005,6 @@ CREATE INDEX dutycycle_state_dutycycle_id_index ON public.dutycycle_state USING 
 
 
 --
--- Name: message_inserted_at_index; Type: INDEX; Schema: public; Owner: -
---
-
-CREATE INDEX message_inserted_at_index ON public.message USING btree (inserted_at);
-
-
---
--- Name: message_keep_for_testing_index; Type: INDEX; Schema: public; Owner: -
---
-
-CREATE INDEX message_keep_for_testing_index ON public.message USING btree (keep_for_testing);
-
-
---
 -- Name: pwm_cmd_acked_index; Type: INDEX; Schema: public; Owner: -
 --
 
@@ -1097,6 +1079,13 @@ CREATE UNIQUE INDEX remote_profile_name_index ON public.remote_profile USING btr
 --
 
 CREATE UNIQUE INDEX sensor_device_index ON public.sensor USING btree (device);
+
+
+--
+-- Name: sensor_device_unique_index; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE UNIQUE INDEX sensor_device_unique_index ON public.sensor_device USING btree (device);
 
 
 --
@@ -1294,5 +1283,5 @@ ALTER TABLE ONLY public.thermostat_profile
 -- PostgreSQL database dump complete
 --
 
-INSERT INTO public."schema_migrations" (version) VALUES (20171217150128), (20171224164529), (20171224225113), (20171228191703), (20171229001359), (20171231182344), (20180101153253), (20180102171624), (20180102175335), (20180217212153), (20180218021213), (20180222165118), (20180222184042), (20180305193804), (20180307143400), (20180517201719), (20180708221600), (20180709181021), (20190308124055), (20190316032007), (20190317155502), (20190320124824), (20190416130912), (20190417011910), (20191018110319), (20191022013914), (20200105131440), (20200115151705), (20200116024319), (20200127033742), (20200128032134), (20200210202655), (20200212175538), (20200212183409), (20200213192845), (20200215173921), (20200217154954), (20200302001850), (20200302155853), (20200309213120), (20200311130709), (20200313132136), (20200314125818), (20200314144615), (20200314152346), (20200314233840), (20200320022913), (20200325211220), (20200506182825), (20200511174457), (20200512174739), (20200512185326), (20200513205755), (20200522043654);
+INSERT INTO public."schema_migrations" (version) VALUES (20171217150128), (20171224164529), (20171224225113), (20171228191703), (20171229001359), (20171231182344), (20180101153253), (20180102171624), (20180102175335), (20180217212153), (20180218021213), (20180222165118), (20180222184042), (20180305193804), (20180307143400), (20180517201719), (20180708221600), (20180709181021), (20190308124055), (20190316032007), (20190317155502), (20190320124824), (20190416130912), (20190417011910), (20191018110319), (20191022013914), (20200105131440), (20200115151705), (20200116024319), (20200127033742), (20200128032134), (20200210202655), (20200212175538), (20200212183409), (20200213192845), (20200215173921), (20200217154954), (20200302001850), (20200302155853), (20200309213120), (20200311130709), (20200313132136), (20200314125818), (20200314144615), (20200314152346), (20200314233840), (20200320022913), (20200325211220), (20200506182825), (20200511174457), (20200512174739), (20200512185326), (20200513205755), (20200522043654), (20200525210412), (20200526171324), (20200526172112), (20200527115635), (20200527161830);
 
