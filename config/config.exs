60fd1f0fc1e39d24548cd17697b5713bf6d6bef4
Tim Hughey <timhughey@mac.com>
2020-07-11 17:23:58 -0400

    Created Phoenix app UI

--- config/config.exs
+++ config/config.exs
@@ -4,35 +4,17 @@ use Mix.Config
 
 config :elixir, :time_zone_database, Tzdata.TimeZoneDatabase
 
-config :helen, ecto_repos: [Repo]
-
 # Configures Elixir's Logger
 config :logger,
   # console: [metadata: [:module], format: "$time $metadata$message\n"],
   console: [format: "$time $message\n"],
   backends: [:console],
   level: :info,
+  metadata: [:request_id],
   compile_time_purge_matching: [
     [application: :helen, level_lower_than: :info],
     [application: :swarm, level_lower_than: :error]
   ]
 
-config :scribe, style: Scribe.Style.GithubMarkdown
-
-# default settings for dev and test, must override in prod
-config :helen,
-  feeds: [
-    prefix: "dev",
-    rpt: {"prod/r/#", 0}
-  ]
-
-config :helen, OTA, [
-  {:uri,
-   [
-     host: "www.wisslanding.com",
-     path: "helen/firmware",
-     file: "latest.bin"
-   ]}
-]
-
-import_config "#{Mix.env()}.exs"
+import_config "helen/config.exs"
+import_config "ui/config.exs"
---
+++ config/ui/config.exs
@@ -0,0 +1,23 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+# Configures the endpoint
+config :ui, UiWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "uHqvJEz8vTo0YlOH4BzlpuJLPvWCjdpbXjdURYPIGlthgj3S79BQU6vzyEhZcBt2",
+  render_errors: [view: UiWeb.ErrorView, accepts: ~w(html json), layout: false],
+  pubsub_server: Ui.PubSub,
+  live_view: [signing_salt: "gcVuZPJL"]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
