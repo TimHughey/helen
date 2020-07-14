76403feef240a2b94d9e7d8456aee32dc59e5960
Tim Hughey <timhughey@mac.com>
2020-07-14 15:44:18 -0400

    Added building/digesting phoenix static assets

--- extra/scripts/prod-build.sh
+++ extra/scripts/prod-build.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env zsh
 pushd -q ${HOME}/devel/helen
 
+env MIX_ENV=prod npm run deploy --prefix ./assets
+env MIX_ENV=prod mix phx.digest
 env MIX_ENV=prod mix release helen --overwrite
