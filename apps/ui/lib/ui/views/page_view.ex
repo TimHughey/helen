dcbfccad625fdffd2f3e9de7c16dc1fc08dbace7
Tim Hughey <timhughey@mac.com>
2020-07-15 14:36:59 -0400

    Simple and somewhat visually pleasing reef state page

--- apps/ui/lib/ui/views/page_view.ex
+++ apps/ui/lib/ui/views/page_view.ex
@@ -2,10 +2,12 @@ defmodule UI.PageView do
   use UI, :view
 
   def reef_status do
+    alias Phoenix.HTML.Format
+
     Reef.x_state()
     |> inspect(pretty: true)
     |> IO.iodata_to_binary()
-    |> Phoenix.HTML.Format.text_to_html()
+    |> Format.text_to_html()
   end
 
   def reef_state do
@@ -16,19 +18,20 @@ defmodule UI.PageView do
     %{worker_mode: mode} = state = Reef.x_state()
     display = get_in(state, [mode]) || %{status: :ready}
 
-    content_tag :section, class: :row, name: "reef_state" do
-      content_tag :table, class: :state_table do
-        content_tag :tbody do
-          for {k, v} <- display do
-            content_tag :tr, class: :state_row do
-              [
-                content_tag(:td, "#{inspect(k)}" |> html_escape(), class: :state_key),
-                content_tag(:td, "#{inspect(v)}" |> html_escape(), class: :state_value)
-              ]
-            end
-          end
-        end
+    # content_tag :section, class: :row, name: "reef_state" do
+    #   content_tag :table, class: :steelBlueCols do
+    #     content_tag :tbody do
+    for {k, v} <- display do
+      content_tag :div, class: "row reef_state_row" do
+        [
+          content_tag(:div, "#{inspect(k)}" |> html_escape(), class: "column reef_state_key"),
+          content_tag(:div, "#{inspect(v)}" |> html_escape(), class: "column reef_state_value")
+        ]
       end
+
+      #     end
+      #   end
+      # end
     end
   end
 end
