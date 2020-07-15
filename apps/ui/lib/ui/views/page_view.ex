8d92306bf398e605b178ef29d903e363338d2023
Tim Hughey <timhughey@mac.com>
2020-07-15 18:37:21 -0400

    Reef UI Improvements

--- apps/ui/lib/ui/views/page_view.ex
+++ apps/ui/lib/ui/views/page_view.ex
@@ -18,22 +18,42 @@ defmodule UI.PageView do
     %{worker_mode: mode} = state = Reef.x_state()
     display = get_in(state, [mode]) || %{status: :ready}
 
-    # content_tag :section, class: :row, name: "reef_state" do
-    #   content_tag :table, class: :steelBlueCols do
-    #     content_tag :tbody do
-    for {k, v} <- display do
+    for {k, v} <- Map.drop(display, [:opts, :step_devices]) do
       content_tag :div, class: "row reef_state_row" do
         [
-          content_tag(:div, "#{Atom.to_string(k)}" |> html_escape(),
-            class: "column reef_state_key"
-          ),
-          content_tag(:div, "#{inspect(v)}" |> html_escape(), class: "column reef_state_value")
+          content_tag(:div, humanize_key(k) |> html_escape(), class: "column reef_state_key"),
+          content_tag(:div, humanize_value(k, v) |> html_escape(),
+            class: "column reef_state_value"
+          )
         ]
       end
-
-      #     end
-      #   end
-      # end
     end
+  end
+
+  defp humanize_key(k) do
+    Atom.to_string(k)
+  end
+
+  defp humanize_value(k, v) do
+    import Helen.Time.Helper, only: [to_binary: 1]
+
+    case k do
+      k when k in [:elapsed, :started_at, :will_finish_by] -> to_binary(v)
+      _k when is_atom(v) -> Atom.to_string(v)
+      _k -> inspect(v)
+    end
+  end
+
+  def worker_mode do
+    %{worker_mode: mode} = Reef.x_state()
+
+    parts = Atom.to_string(mode) |> String.split("_")
+
+    for p <- parts do
+      String.capitalize(p)
+    end
+    |> Enum.join(" ")
+    |> IO.iodata_to_binary()
+    |> html_escape()
   end
 end
