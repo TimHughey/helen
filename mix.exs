8f5c4f37401924d91b0ecd1728bdddfa6779ee00
Tim Hughey <timhughey@mac.com>
2020-09-12 20:17:50 -0400

    Version Bump

--- mix.exs
+++ mix.exs
@@ -1,10 +1,12 @@
 defmodule FatHelen.MixProject do
+  @moduledoc false
+
   use Mix.Project
 
   def project do
     [
       apps_path: "apps",
-      version: "0.1.0",
+      version: "0.1.1",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       releases: releases()
