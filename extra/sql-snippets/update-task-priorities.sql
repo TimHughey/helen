193eb285f49b1d23cf1994453ba6ae08f8e96fe0
Tim Hughey <timhughey@mac.com>
2020-10-27 18:25:36 -0400

    Minor Bug Fixes

---
+++ extra/sql-snippets/update-task-priorities.sql
@@ -0,0 +1,26 @@
+UPDATE
+    remote_profile
+SET
+    (i2c_discover_priority,
+        i2c_report_priority,
+        i2c_command_priority) = (4,
+        5,
+        11);
+
+UPDATE
+    remote_profile
+SET
+    (pwm_report_priority,
+        pwm_command_priority) = (5,
+        11);
+
+UPDATE
+    remote_profile
+SET
+    (dalsemi_discover_priority,
+        dalsemi_convert_priority,
+        dalsemi_report_priority,
+        dalsemi_command_priority) = (4,
+        5,
+        5,
+        11);
