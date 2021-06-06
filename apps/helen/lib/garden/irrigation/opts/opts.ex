06e6ede4833c6e43da2f00a9a2bcde75b43e8ea2
Tim Hughey <timhughey@mac.com>
2021-06-06 14:18:42 -0400

    Adjusted Irrigation schedule

--- apps/helen/lib/garden/irrigation/opts/opts.ex
+++ apps/helen/lib/garden/irrigation/opts/opts.ex
@@ -8,7 +8,7 @@ defmodule Garden.Irrigation.Opts do
           device: "irrigation flower boxes",
           schedule: [am: "PT2M", noon: "PT1M", pm: "PT1M"]
         ],
-        garden: [device: "irrigation garden", schedule: [am: "PT10M", noon: "PT5M", pm: "PT10M"]]
+        garden: [device: "irrigation garden", schedule: [am: "PT10M", noon: "PT5M", pm: "PT3M"]]
       ],
       power: [device: "irrigation 12v power", power_up_delay: "PT5S"],
       device_group: "irrigation",
