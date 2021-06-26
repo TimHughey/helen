58bdef90822e35de9e85d7718789ddde41a2a047
Tim Hughey <timhughey@mac.com>
2021-06-26 09:43:41 -0400

    Sally added make_alias/4 doc

--- apps/sally/lib/sally.ex
+++ apps/sally/lib/sally.ex
@@ -6,8 +6,23 @@ defmodule Sally do
   alias Alfred.ExecCmd
   alias Sally.{DevAlias, Device}
 
+  # required as callback from Alfred
   def execute(%ExecCmd{} = ec), do: Sally.Execute.cmd(ec)
 
+  @doc """
+    Create an alias to a device and pio
+
+    ```elixir
+
+    Sally.make_alias(alias_name, device_ident, pio, [ttl_ms: 15_000, description: "new dev alias"])
+
+    ```
+
+    **NOTE**
+    Device alias names must be unique across *all* device types.
+
+  """
+  @doc since: "0.5.2"
   def make_alias(name, dev_ident, pio, opts \\ [])
       when is_binary(name) and is_binary(dev_ident) and pio >= 0 do
     opts_map = Keyword.take(opts, [:ttl_ms, :description]) |> Enum.into(%{})
@@ -19,5 +34,6 @@ defmodule Sally do
     end
   end
 
+  # required as callback from Alfred
   def status(name, opts \\ []), do: Sally.Status.get(name, opts)
 end
