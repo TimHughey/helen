71b58d098d841734d6fc651fb4213f87474bf3c7
Tim Hughey <timhughey@mac.com>
2021-07-27 07:07:07 -0400

    Normalized Reef configuration env variables

--- apps/reef/lib/reef.ex
+++ apps/reef/lib/reef.ex
@@ -2,11 +2,12 @@ defmodule NewReef do
   require Logger
   alias NewReef.Config
 
-  def parse_config(file) do
-    base_path = System.get_env("REEF_CONFIGS")
+  def parse_config do
+    base_path = System.get_env("REEF_CONFIG_PATH") || "."
+    cfg_file = System.get_env("REEF_CONFIG_FILE") || "defaults_v2.txt"
 
-    _config = Config.parse(base_path, file)
+    config = Config.parse(base_path, cfg_file)
 
-    # Logger.info("\n#{inspect(config, pretty: true)}")
+    Logger.info("\n#{inspect(config, pretty: true)}")
   end
 end
