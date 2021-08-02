8ccfe0f05eb96774eaf42e77712200a6ee1882d0
Tim Hughey <timhughey@mac.com>
2021-08-02 17:47:49 -0400

    Simplified Sally.Host.Firmware

--- apps/sally/lib/sally/host/firmware.ex
+++ apps/sally/lib/sally/host/firmware.ex
@@ -1,26 +1,16 @@
 defmodule Sally.Host.Firmware do
-  alias __MODULE__
   alias Sally.Host
   alias Sally.Host.Instruct
 
-  @firmware_host Application.compile_env!(:sally, [Firmware, :uri, :host])
-  @firmware_path Application.compile_env!(:sally, [Firmware, :uri, :path])
-
-  def ota(name, firmware_file) when is_binary(name) do
+  def ota(name, opts \\ []) when is_list(opts) do
     case Host.find_by_name(name) do
-      %Host{} = host -> ota(host, firmware_file)
-      nil -> {:not_found, name}
+      %Host{} = host ->
+        data = %{valid_ms: opts[:valid_ms] || 60_000, file: opts[:file] || "latest.bin"}
+
+        Instruct.send(ident: host.ident, subsystem: "host", data: data, filters: ["ota"])
+
+      nil ->
+        {:not_found, name}
     end
-  end
-
-  def ota(%Host{} = h, file) do
-    firmware_uri = [@firmware_path, file] |> Path.join()
-
-    Instruct.send(
-      ident: h.ident,
-      subsystem: "host",
-      data: %{uri: firmware_uri, src_host: @firmware_host},
-      filters: ["ota"]
-    )
   end
 end
