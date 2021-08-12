87218c7a8bd86060273605d6b765bb7aec860c5a
Tim Hughey <timhughey@mac.com>
2021-08-12 18:29:17 -0400

    Corrected test exports

--- extra/bin/test-export.sh
+++ extra/bin/test-export.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env zsh
 
 export MIX_ENV=test
-export EVA_CONFIG_PATH=${HOME}/devel/shell/local/eva/toml
-export EVA_CONFIG_FILE=timed_cmd.toml
-export EVA_AUTOFF_CONFIG_FILE=autooff.toml
+export HELEN_BASE=${HOME}/devel/helen
