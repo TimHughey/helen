4a9eb92816c5242aea2bd4ab39a0c1a03f0212e1
Tim Hughey <timhughey@mac.com>
2021-08-14 14:35:07 -0400

    Removed Eva legacy reference implementation config

--- config/eva/config.exs
+++ config/eva/config.exs
@@ -1,7 +1,6 @@
 import Config
 
-if Config.config_env() == :test do
-  config :eva, Eva.Habitat, cfg_file: "apps/eva/test/reference_impl/toml/timed_cmd.toml"
+if Config.config_env() in [:dev, :test] do
   config :eva, Eva.RefImpl.AutoOff, cfg_file: "apps/eva/test/reference_impl/toml/autooff.toml"
   config :eva, Eva.RefImpl.RuthLED, cfg_file: "apps/eva/test/reference_impl/toml/ruth_led.toml"
 end
