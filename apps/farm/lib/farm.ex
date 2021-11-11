6ddab7d85abb6fcbf71c5b2e144d88cf85731ea0
Tim Hughey <timhughey@mac.com>
2021-11-11 16:48:00 -0500

    Corrected Farm Circulation DevAlias

--- apps/farm/lib/farm.ex
+++ apps/farm/lib/farm.ex
@@ -7,14 +7,14 @@ defmodule Farm do
     alias Alfred.ExecCmd
 
     child_id = Farm.Womb
-    heater = "womb heater power"
-    ec = %ExecCmd{name: heater, cmd: "25% of max", cmd_params: %{type: "fixed", percent: 25}}
+    circulation = "womb circulation pwm"
+    ec = %ExecCmd{name: circulation, cmd: "25% of max", cmd_params: %{type: "fixed", percent: 25}}
 
     available_actions = [:circulation_on, :circulation_off, :restart, :state, :terminate]
 
     case action do
       :circulation_on -> Alfred.execute(ec)
-      :circulation_off -> Alfred.off(heater)
+      :circulation_off -> Alfred.off(circulation)
       :restart -> Supervisor.restart_child(Farm.Supervisor, child_id)
       :state -> :sys.get_state(child_id)
       :terminate -> Supervisor.terminate_child(Farm.Supervisor, child_id)
