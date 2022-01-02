402374bc8634e63a745b32049514dada03536fc6
Tim Hughey <timhughey@mac.com>
2022-01-02 12:01:57 -0500

    Adjusted Farm startup config
     -continuing to sort out Carol execute args merge

--- config/farm/config.exs
+++ config/farm/config.exs
@@ -10,7 +10,7 @@ config :farm, Farm,
     womb_circulation: [
       # NOTE: the cmd is specified in defaults for the single episode
       equipment: "womb circulation pwm",
-      defaults: [execute: [cmd: "25% of max", params: [type: "fixed", percent: 25]]],
-      episodes: [[id: "All Day", event: "beginning of day"]]
+      defaults: [execute: [params: [type: "fixed", percent: 25]]],
+      episodes: [[id: "All Day", event: "beginning of day"], execute: [cmd: "25% of max"]]
     ]
   ]
