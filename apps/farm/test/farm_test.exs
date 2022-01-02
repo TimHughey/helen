d5206eac702a1df7aa59477fb1342e4089de9a23
Tim Hughey <timhughey@mac.com>
2022-01-02 12:56:08 -0500

    Moved reusable assert_started/2 to Should
     -Farm uses Should.assert_started/2

--- apps/farm/test/farm_test.exs
+++ apps/farm/test/farm_test.exs
@@ -1,22 +1,7 @@
 defmodule FarmTest do
   use ExUnit.Case, async: true
   use Carol.AssertAid, module: Farm
-
-  defmacro assert_started(server_name, opts) do
-    quote bind_quoted: [server_name: server_name, opts: opts] do
-      sleep = opts[:sleep] || 10
-      reductions = opts[:attempts] || 1
-
-      assert Enum.reduce(1..reductions, :check, fn
-               _x, :check -> GenServer.whereis(server_name)
-               _x, pid when is_pid(pid) -> Process.alive?(pid)
-               _x, false -> Process.sleep(sleep) && :check
-               _x, true -> true
-             end)
-
-      assert GenServer.whereis(server_name)
-    end
-  end
+  import Should, only: [assert_started: 2]
 
   describe "Womb.Farm.Heater" do
     test "starts" do
--- apps/should/lib/should.ex
+++ apps/should/lib/should.ex
@@ -1,7 +1,29 @@
 defmodule Should do
-  defmacro __using__(_opts) do
+  defmacro __using__(_use_opts) do
     quote do
+      require Should
       import Should, only: [pretty_puts: 1]
+    end
+  end
+
+  @doc """
+  Assert a GenServer has started (using `server_name`)
+
+  """
+  @doc since: "0.7.1"
+  defmacro assert_started(server_name, opts) do
+    quote bind_quoted: [server_name: server_name, opts: opts] do
+      sleep = opts[:sleep] || 10
+      reductions = opts[:attempts] || 1
+
+      assert Enum.reduce(1..reductions, :check, fn
+               _x, :check -> GenServer.whereis(server_name)
+               _x, pid when is_pid(pid) -> Process.alive?(pid)
+               _x, false -> Process.sleep(sleep) && :check
+               _x, true -> true
+             end)
+
+      assert GenServer.whereis(server_name)
     end
   end
 
@@ -12,6 +34,7 @@ defmodule Should do
   tap(x, -> IO.puts(["\\n", inspect(x, pretty: true)])
   ```
   """
+  @doc since: "0.1.0"
   defmacro pretty_puts(x) do
     quote bind_quoted: [x: x] do
       tap(x, fn x -> IO.puts(["\n", inspect(x, pretty: true)]) end)
