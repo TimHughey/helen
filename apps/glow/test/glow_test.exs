b20415fa4855de9eb97ba4497fa8d245acef1215
Tim Hughey <timhughey@mac.com>
2022-01-02 10:39:22 -0500

    Glow Unit Test updates
     -use Carol.AssertAid to avoid duplicated unit test code

--- apps/glow/test/glow_test.exs
+++ apps/glow/test/glow_test.exs
@@ -1,18 +1,8 @@
 defmodule GlowTest do
   use ExUnit.Case
+  use Carol.AssertAid, module: Glow
 
   @moduletag glow: true, glow_base: true
 
-  describe "Glow.children/0" do
-    test "returns list of children" do
-      children = Glow.which_children()
-
-      assert [{_, _, _, _} = _child1, _child2, _child3, _child4] = children
-
-      for {id, pid, _type, _module} <- children do
-        assert ["Glow" | _] = Module.split(id)
-        assert is_pid(pid)
-      end
-    end
-  end
+  # NOTE: required tests are provided by Carol.AssertAid
 end
