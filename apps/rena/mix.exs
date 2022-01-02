c4589dee075702e193886b26bea228a7a035084b
Tim Hughey <timhughey@mac.com>
2022-01-02 10:44:30 -0500

    Retired Rena.HoldCmd
     -Carol application provides the same functionality

--- apps/rena/mix.exs
+++ apps/rena/mix.exs
@@ -4,7 +4,7 @@ defmodule Rena.MixProject do
   def project do
     [
       app: :rena,
-      version: "0.1.12",
+      version: "0.2.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
