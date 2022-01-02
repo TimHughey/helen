5f2905af561f4cade399a4d99e208ed150620e6c
Tim Hughey <timhughey@mac.com>
2022-01-02 08:55:39 -0500

    Glow commented code clean-up

--- apps/glow/lib/glow/application.ex
+++ apps/glow/lib/glow/application.ex
@@ -3,16 +3,9 @@ defmodule Glow.Application do
 
   use Application
 
-  # alias Glow.Instance
-
   @impl true
   def start(_type, _args) do
     children = [Glow]
-    #   {Carol.Server, Instance.start_args(:front_chandelier)},
-    #   {Carol.Server, Instance.start_args(:front_evergreen)},
-    #   {Carol.Server, Instance.start_args(:front_red_maple)},
-    #   {Carol.Server, Instance.start_args(:greenhouse)}
-    # ]
 
     # start Glow.Application supervisor
     opts = [strategy: :one_for_one, max_restarts: 10, max_seconds: 10]
