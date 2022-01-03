f75dca77e0556e208a114d64bd3e9791f579be1d
Tim Hughey <timhughey@mac.com>
2022-01-03 08:53:10 -0500

    Adjusted Glow startup config
     -ruth requires a code change to ignore cmd 'type' param when cmd is 'on' or 'off'

--- apps/glow/test/glow_test.exs
+++ apps/glow/test/glow_test.exs
@@ -1,8 +1,27 @@
 defmodule GlowTest do
   use ExUnit.Case
   use Carol.AssertAid, module: Glow
+  use Should
 
   @moduletag glow: true, glow_base: true
 
   # NOTE: required tests are provided by Carol.AssertAid
+
+  describe "Glow instance execute args" do
+    test "validate front chandelier" do
+      episodes = Glow.state(:chan, :episodes)
+
+      ec =
+        Carol.Episode.execute_args([equipment: "some name"], :active, episodes)
+        |> Alfred.ExecCmd.Args.auto()
+        |> Alfred.ExecCmd.new()
+
+      assert %Alfred.ExecCmd{
+               cmd: "Overnight",
+               cmd_opts: [],
+               cmd_params: %{min: _, max: _, primes: _, step: _, step_ms: _, type: "random"},
+               name: "some name"
+             } = ec
+    end
+  end
 end
--- config/glow/config.exs
+++ config/glow/config.exs
@@ -9,10 +9,10 @@ config :glow, Glow,
   instances: [
     front_chandelier: [
       equipment: "front chandelier pwm",
-      defaults: [execute: [params: [type: "random", primes: 8, step: 6, step_ms: 40]]],
+      defaults: [execute: [params: [primes: 8, step: 6, step_ms: 40]]],
       episodes: [
-        [id: "Evening", event: "sunset", execute: [params: [min: 384, max: 1024]]],
-        [id: "Overnight", event: "astro set", execute: [params: [min: 175, max: 640]]],
+        [id: "Evening", event: "sunset", execute: [type: "random", params: [min: 384, max: 1024]]],
+        [id: "Overnight", event: "astro set", execute: [type: "random", params: [min: 175, max: 640]]],
         [id: "Day", event: "civil rise", execute: [cmd: "off"]]
       ]
     ],
@@ -20,8 +20,8 @@ config :glow, Glow,
       equipment: "front evergreen pwm",
       defaults: [execute: [params: [type: "random", primes: 8, step: 12, step_ms: 40]]],
       episodes: [
-        [id: "Evening", event: "sunset", execute: [params: [min: 384, max: 3072]]],
-        [id: "Overnight", event: "astro set", execute: [params: [min: 175, max: 2560]]],
+        [id: "Evening", event: "sunset", execute: [type: "random", params: [min: 384, max: 3072]]],
+        [id: "Overnight", event: "astro set", execute: [type: "random", params: [min: 175, max: 2560]]],
         [id: "Day", event: "civil rise", execute: [cmd: "off"]]
       ]
     ],
@@ -29,8 +29,8 @@ config :glow, Glow,
       equipment: "front red maple pwm",
       defaults: [execute: [params: [type: "random", primes: 8, step: 12, step_ms: 40]]],
       episodes: [
-        [id: "Evening", event: "sunset", execute: [params: [min: 384, max: 3072]]],
-        [id: "Overnight", event: "astro set", execute: [params: [min: 175, max: 2560]]],
+        [id: "Evening", event: "sunset", execute: [type: "random", params: [min: 384, max: 3072]]],
+        [id: "Overnight", event: "astro set", execute: [type: "random", params: [min: 175, max: 2560]]],
         [id: "Day", event: "civil rise", execute: [cmd: "off"]]
       ]
     ],
