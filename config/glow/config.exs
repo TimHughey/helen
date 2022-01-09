0b7990c4dec70cad94384797d40f4167e3659bd8
Tim Hughey <timhughey@mac.com>
2022-01-09 17:58:05 -0500

    Adjust Glow Startup Config

--- config/glow/config.exs
+++ config/glow/config.exs
@@ -11,8 +11,8 @@ config :glow, Glow,
       equipment: "front chandelier pwm",
       defaults: [execute: [params: [primes: 8, step: 6, step_ms: 40]]],
       episodes: [
-        [id: "Evening", event: "sunset", execute: [type: "random", params: [min: 384, max: 1024]]],
-        [id: "Overnight", event: "astro set", execute: [type: "random", params: [min: 175, max: 640]]],
+        [id: "Evening", event: "sunset", execute: [params: [type: "random", min: 384, max: 1024]]],
+        [id: "Overnight", event: "astro set", execute: [params: [type: "random", min: 175, max: 640]]],
         [id: "Day", event: "civil rise", execute: [cmd: "off"]]
       ]
     ],
@@ -20,8 +20,8 @@ config :glow, Glow,
       equipment: "front evergreen pwm",
       defaults: [execute: [params: [step: 12, step_ms: 40]]],
       episodes: [
-        [id: "Evening", event: "sunset", execute: [type: "random", params: [min: 384, max: 3072]]],
-        [id: "Overnight", event: "astro set", execute: [type: "random", params: [min: 175, max: 2560]]],
+        [id: "Evening", event: "sunset", execute: [params: [type: "random", min: 384, max: 3072]]],
+        [id: "Overnight", event: "astro set", execute: [params: [type: "random", min: 175, max: 2560]]],
         [id: "Day", event: "civil rise", execute: [cmd: "off"]]
       ]
     ],
@@ -29,8 +29,8 @@ config :glow, Glow,
       equipment: "front red maple pwm",
       defaults: [execute: [params: [primes: 8, step: 12, step_ms: 40]]],
       episodes: [
-        [id: "Evening", event: "sunset", execute: [type: "random", params: [min: 384, max: 3072]]],
-        [id: "Overnight", event: "astro set", execute: [type: "random", params: [min: 175, max: 2560]]],
+        [id: "Evening", event: "sunset", execute: [params: [type: "random", min: 384, max: 3072]]],
+        [id: "Overnight", event: "astro set", execute: [params: [type: "random", min: 175, max: 2560]]],
         [id: "Day", event: "civil rise", execute: [cmd: "off"]]
       ]
     ],
--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Helen.MixProject do
 
   use Mix.Project
 
-  @vsn "2.1.30"
+  @vsn "2.1.31"
 
   def project do
     [
