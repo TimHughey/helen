3d2c47660f037e8ee419c5d14bed860ef55eedbd
Tim Hughey <timhughey@mac.com>
2022-03-06 16:02:09 -0500

    Refining Farm Womb adjust when

--- apps/farm/lib/farm/womb_heater.ex
+++ apps/farm/lib/farm/womb_heater.ex
@@ -4,9 +4,9 @@ defmodule Farm.Womb.Heater do
     equipment: "womb heater power",
     sensor_group: [
       names: ["womb 1", "womb 2", "womb 3", "womb 4"],
-      range: [high: 80.3, low: 78.7, unit: :temp_f],
+      range: [high: 80.0, low: 78.0, unit: :temp_f],
       # NOTE: Rena provides [lower: [gt_high: 1], raise: [lt_low: 1]] by default
-      adjust_when: [lower: [gt_high: 1, gt_mid: 1, lt_mid: 2], raise: [lt_low: 1]],
+      adjust_when: [lower: [gt_high: 1, gt_mid: 1, lt_mid: 1], raise: [lt_low: 1]],
       valid_when: [valid: 3, total: 3]
     ]
 end
